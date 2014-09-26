55b982eea507601fc87c329e7cbf5cbc79ee2da2
Brian Hechinger <bhechinger@cardiomems.com>
2014-09-26 16:47:43 -0400

    pulls data from the actual static db and returns it as XML

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import kxml.xml;
 import mysql.connection;
 import std.stdio;
 import mysql.db;
+import std.conv;
 
 shared static this() {
 	auto settings = new HTTPServerSettings;
@@ -16,16 +17,21 @@ shared static this() {
 void hello(HTTPServerRequest req, HTTPServerResponse res) {
   // CREATE USER 'eve_static'@'localhost' IDENTIFIED BY 'eve_static';
   // GRANT ALL PRIVILEGES ON eve_static.* TO 'eve_static'@'localhost'
-  string DSN = "host=localhost;port=3306;user=eve_static;pwd=eve_static;db=test";
+  string DSN = "host=localhost;port=3306;user=eve_static;pwd=eve_static;db=eve_static";
   auto mdb = new MysqlDB(DSN);
   auto c = mdb.lockConnection();
   scope(exit) c.close();
 
-  auto command = new Command(c, "SELECT name, value FROM foo");
+  auto command = new Command(c, "SELECT combatZoneID, combatZoneName, factionID, centerSystemID, description FROM warCombatZones");
   auto results = command.execSQLResult();
-  XmlNode node = new XmlNode("thing");
+  XmlNode node = new XmlNode("warCombatZones");
   foreach (row; results) {
-    node.addChild(new XmlNode(row[0].get!string).addCData(row[1].get!string));
+    XmlNode foo = new XmlNode(row[1].get!string);
+    foo.addChild(new XmlNode("description").addCData(row[4].get!string));
+    foo.addChild(new XmlNode("combatZoneID").addCData(to!string(row[0].get!int)));
+    foo.addChild(new XmlNode("factionID").addCData(to!string(row[2].get!int)));
+    foo.addChild(new XmlNode("centerSystemID").addCData(to!string(row[3].get!int)));
+    node.addChild(foo);
   }
 	res.writeBody(node.toPrettyString);
 }
