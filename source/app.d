a14aa165aabbe0f41a8f106531dff2591f9508ed
Brian Hechinger <bhechinger@cardiomems.com>
2014-09-28 17:44:41 -0400

    some basic mysql error handling done

--- source/app.d
+++ source/app.d
@@ -5,37 +5,99 @@ import std.stdio;
 import mysql.db;
 import std.conv;
 
+MysqlDB mdb;
+
 shared static this() {
 	auto settings = new HTTPServerSettings;
 	settings.port = 8181;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+
+  // CREATE USER 'eve_static'@'localhost' IDENTIFIED BY 'eve_static';
+  // GRANT ALL PRIVILEGES ON eve_static.* TO 'eve_static'@'localhost'
+  try {
+    string DSN = "host=localhost;port=3306;user=eve_static;pwd=eve_static;db=eve_static";
+    mdb = new MysqlDB(DSN);
+  } catch (Exception e1) {
+    // Now what, though. How do we inform the client?
+    logError("Exception: " ~ e1.msg);
+  }
+
+  auto router = new URLRouter;
+  router.get("/tables/list", &getTableList);
+  router.get("/table/:tableName", &getTable);
+  router.get("/columns/:tableName", &getColumnList);
+	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8181/ in your browser.");
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res) {
-  // CREATE USER 'eve_static'@'localhost' IDENTIFIED BY 'eve_static';
-  // GRANT ALL PRIVILEGES ON eve_static.* TO 'eve_static'@'localhost'
-  string table = "warCombatZones";
-  bool table_found = false;
-  string DSN = "host=localhost;port=3306;user=eve_static;pwd=eve_static;db=eve_static";
-  auto mdb = new MysqlDB(DSN);
+void getTableList(HTTPServerRequest req, HTTPServerResponse res) {
   auto c = mdb.lockConnection();
   scope(exit) c.close();
+  XmlNode root = new XmlNode("eve_static").setAttribute("db_version", "hyperion").setAttribute("error", false);
+
+  try {
+    MetaData md = MetaData(c);
+    auto curTables = md.tables();
+    XmlNode tables = new XmlNode("tables");
+    foreach(tbls ; curTables) {
+      tables.addChild(new XmlNode(tbls));
+    }
+    root.addChild(tables);
+  } catch (Exception e1) {
+    root.setAttribute("error", true);
+    root.addChild(new XmlNode("error").addCData(e1.msg));
+    logError("Exception: " ~ e1.msg);
+  }
+	res.writeBody(root.toPrettyString);
+}
+
+void getColumnList(HTTPServerRequest req, HTTPServerResponse res) {
+  string table = req.params["tableName"];
+  auto c = mdb.lockConnection();
+  scope(exit) c.close();
+  XmlNode root = new XmlNode("eve_static").setAttribute("db_version", "hyperion").setAttribute("error", false);
+
+  try {
+    MetaData md = MetaData(c);
+    auto curColumns = md.columns(table);
+    XmlNode columns = new XmlNode("columns").setAttribute("table", table);
+    foreach(cols; curColumns) {
+      columns.addChild(new XmlNode(cols.name));
+    }
+    root.addChild(columns);
+  } catch (Exception e1) {
+    root.setAttribute("error", true);
+    root.addChild(new XmlNode("error").addCData(e1.msg));
+    logError("Exception: " ~ e1.msg);
+  }
+	res.writeBody(root.toPrettyString);
+}
+
+void getTable(HTTPServerRequest req, HTTPServerResponse res) {
+  string table = req.params["tableName"];
+  bool table_found = false;
+  auto c = mdb.lockConnection();
+  scope(exit) c.close();
+  MetaData md;
 
   XmlNode root = new XmlNode("eve_static").setAttribute("db_version", "hyperion").setAttribute("error", false);
 
-  MetaData md = MetaData(c);
-  auto curTables = md.tables();
-  XmlNode tables = new XmlNode("tables");
-  foreach(tbls ; curTables) {
-    if (table == tbls) {
+  try {
+    md = MetaData(c);
+    auto curTables = md.tables();
+    foreach(tbls ; curTables) {
+      if (table == tbls) {
         table_found = true;
+      }
     }
-    tables.addChild(new XmlNode(tbls));
+  } catch (Exception e1) {
+    root.setAttribute("error", true);
+    root.addChild(new XmlNode("error").addCData(e1.msg));
+    logError("Exception: " ~ e1.msg);
+    res.writeBody(root.toPrettyString);
+    return;
   }
-  //root.addChild(tables);
 
   if (!table_found) {
     root.setAttribute("error", true);
@@ -44,16 +106,22 @@ void hello(HTTPServerRequest req, HTTPServerResponse res) {
     return;
   }
 
-  auto curColumns = md.columns(table);
-  XmlNode columns = new XmlNode("columns");
-  foreach(cols; curColumns) {
-      columns.addChild(new XmlNode(cols.name));
+  ResultSet results;
+  ColumnInfo[] curColumns;
+  try {
+    curColumns = md.columns(table);
+    //pragma(msg, typeof(curColumns))
+    auto command = new Command(c);
+    command.sql = "SELECT * FROM " ~ table;
+    results = command.execSQLResult();
+  } catch (Exception e1) {
+    root.setAttribute("error", true);
+    root.addChild(new XmlNode("error").addCData(e1.msg));
+    logError("Exception: " ~ e1.msg);
+    res.writeBody(root.toPrettyString);
+    return;
   }
-  //root.addChild(columns);
 
-  auto command = new Command(c);
-  command.sql = "SELECT * FROM " ~ table;
-  auto results = command.execSQLResult();
   XmlNode node = new XmlNode(table).setAttribute("rowsReturned", results.length);
 
   foreach (row; results) {
