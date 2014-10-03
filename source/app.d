a7c7b2f494cedd29e843bb7ff38f02bbb2db8979
Brian Hechinger <bhechinger@cardiomems.com>
2014-10-03 16:37:52 -0400

    major overhaul including outputting of text and XML formats

--- source/app.d
+++ source/app.d
@@ -53,8 +53,19 @@ shared static this() {
 
   auto router = new URLRouter;
   router.get("/tables/list", &getTableList);
+  router.get("/tables/list/:format", &getTableList);
+
   router.get("/table/:tableName", &getTable);
+
   router.get("/columns/:tableName", &getColumnList);
+  router.get("/columns/:tableName/:format", &getColumnList);
+
+  router.get("/item/lookup/Name/:itemID", &lookupItem);
+  router.get("/item/lookup/Name/:itemID/:format", &lookupItem);
+
+  router.get("/item/lookup/ID/:itemName", &lookupItem);
+  router.get("/item/lookup/ID/:itemName/:format", &lookupItem);
+
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8181/ in your browser.");
@@ -64,13 +75,39 @@ XmlNode createRootElement() {
   return(new XmlNode("eve_static").setAttribute("db_version", db_version).setAttribute("error", false));
 }
 
-string sendErrorResponse(string msg) {
+string getFormat(HTTPServerRequest req) {
+  string valid_formats[] = ["text", "xml"];
+
+  try {
+    foreach(fmt; valid_formats) {
+      string format = req.params["format"];
+      if (fmt == format) {
+        return(format);
+      }
+    }
+  } catch (RangeError) {
+    // Fallthrough to XML
+  }
+  return("xml");
+}
+
+string getErrorResponse(string msg, string format) {
   refresh_db = true;
-  XmlNode root = createRootElement();
-  root.setAttribute("error", true);
-  root.addChild(new XmlNode("error").addCData(msg));
-  logError("Exception: " ~ msg);
-  return(root.toPrettyString);
+
+  // Replace this with a case statement
+  switch (format) {
+    case "xml":
+    default:
+      // XML is the default
+      XmlNode root = createRootElement();
+      root.setAttribute("error", true);
+      root.addChild(new XmlNode("error").addCData(msg));
+      logError("Exception: " ~ msg);
+      return(root.toPrettyString);
+
+    case "text":
+      return("ERROR: " ~ msg);
+  }
 }
 
 void getTableList(HTTPServerRequest req, HTTPServerResponse res) {
@@ -81,14 +118,27 @@ void getTableList(HTTPServerRequest req, HTTPServerResponse res) {
     c = getDBConnection();
     scope(exit) c.close();
 
-    XmlNode tables = new XmlNode("tables");
-    foreach(tbls ; curTables) {
-      tables.addChild(new XmlNode(tbls));
+    switch (getFormat(req)) {
+      case "xml":
+      default:
+        XmlNode tables = new XmlNode("tables");
+        foreach(tbls; curTables) {
+          tables.addChild(new XmlNode(tbls));
+        }
+        root.addChild(tables);
+	      res.writeBody(root.toPrettyString);
+        break;
+
+      case "text":
+        string output;
+        foreach(tbls; curTables) {
+          output ~= tbls ~ "\n";
+        }
+        res.writeBody(output);
+        break;
     }
-    root.addChild(tables);
-	  res.writeBody(root.toPrettyString);
   } catch (Exception e1) {
-    res.writeBody(sendErrorResponse(e1.msg));
+    res.writeBody(getErrorResponse(e1.msg, getFormat(req)));
   }
 }
 
@@ -97,15 +147,31 @@ void getColumnList(HTTPServerRequest req, HTTPServerResponse res) {
   XmlNode root = createRootElement();
 
   try {
+    // This isn't really needed for any other reason than to jog the DB connection if needed.
+    getDBConnection();
     auto curColumns = md.columns(table);
-    XmlNode columns = new XmlNode("columns").setAttribute("table", table);
-    foreach(cols; curColumns) {
-      columns.addChild(new XmlNode(cols.name));
+
+    switch (getFormat(req)) {
+      case "xml":
+      default:
+        XmlNode columns = new XmlNode("columns").setAttribute("table", table);
+        foreach(cols; curColumns) {
+          columns.addChild(new XmlNode(cols.name));
+        }
+        root.addChild(columns);
+	      res.writeBody(root.toPrettyString);
+        break;
+
+      case "text":
+        string output;
+        foreach(cols; curColumns) {
+          output ~= cols.name ~ "\n";
+        }
+        res.writeBody(output);
+        break;
     }
-    root.addChild(columns);
-	  res.writeBody(root.toPrettyString);
   } catch (Exception e1) {
-    res.writeBody(sendErrorResponse(e1.msg));
+    res.writeBody(getErrorResponse(e1.msg, getFormat(req)));
   }
 }
 
@@ -113,7 +179,7 @@ void getTable(HTTPServerRequest req, HTTPServerResponse res) {
   string table = req.params["tableName"];
   bool table_found = false;
   Connection c;
-  XmlNode root = new XmlNode("eve_static").setAttribute("db_version", db_version).setAttribute("error", false);
+  XmlNode root = createRootElement();
 
   try {
     c = getDBConnection();
@@ -125,12 +191,12 @@ void getTable(HTTPServerRequest req, HTTPServerResponse res) {
       }
     }
   } catch (Exception e1) {
-    res.writeBody(sendErrorResponse(e1.msg));
+    res.writeBody(getErrorResponse(e1.msg, "xml"));
     return;
   }
 
   if (!table_found) {
-    res.writeBody(sendErrorResponse("No such table: " ~ table));
+    res.writeBody(getErrorResponse("No such table: " ~ table, "xml"));
     return;
   }
 
@@ -142,8 +208,11 @@ void getTable(HTTPServerRequest req, HTTPServerResponse res) {
     auto command = new Command(c);
     command.sql = "SELECT * FROM " ~ table;
     results = command.execSQLResult();
+  } catch (AssertError e1) {
+    res.writeBody(getErrorResponse(e1.msg ~ ": This is a known error with native-mysql. Waiting for it to be fixed.", "xml"));
+    return;
   } catch (Exception e1) {
-    res.writeBody(sendErrorResponse(e1.msg));
+    res.writeBody(getErrorResponse(e1.msg, "xml"));
     return;
   }
 
@@ -165,4 +234,76 @@ void getTable(HTTPServerRequest req, HTTPServerResponse res) {
 
   root.addChild(node);
 	res.writeBody(root.toPrettyString);
+}
+
+void lookupItem(HTTPServerRequest req, HTTPServerResponse res) {
+  enum { ID, NAME }
+  int action, itemID;
+  string output, itemName;
+  string node_name, node_attr, node_attr_val;
+  Connection c;
+  XmlNode root = createRootElement;
+
+  try {
+    c = getDBConnection();
+    scope(exit) c.close();
+  } catch (Exception e1) {
+    res.writeBody(getErrorResponse(e1.msg, getFormat(req)));
+    return;
+  }
+
+  ResultSet results;
+  try {
+    itemID = req.params["itemID"].to!int;
+    action = ID;
+  } catch (RangeError) {
+    // ignoring
+  }
+  try {
+    itemName = req.params["itemName"];
+    action = NAME;
+  } catch (RangeError) {
+    // ignoring
+  }
+
+  auto command = new Command(c);
+  switch (action) {
+    case ID:
+      command.sql = "SELECT itemName FROM invNames WHERE itemID = ?";
+      command.prepare();
+      command.bindParameter(itemID, 0);
+      results = command.execPreparedResult();
+      output = results[0][0].get!string;
+      node_name = "itemName";
+      node_attr = "id";
+      node_attr_val = itemID.to!string;
+      break;
+
+    case NAME:
+      command.sql = "SELECT itemID FROM invNames WHERE itemName = ?";
+      command.prepare();
+      command.bindParameter(itemName, 0);
+      results = command.execPreparedResult();
+      output = results[0][0].to!string;
+      node_name = "itemID";
+      node_attr = "name";
+      node_attr_val = itemName;
+      break;
+
+    default:
+      res.writeBody(getErrorResponse("This REALLY shouldn't happen, but action is: " ~ action.to!string, getFormat(req)));
+      return;
+  }
+
+  switch (getFormat(req)) {
+    case "xml":
+    default:
+      root.addChild(new XmlNode(node_name).setAttribute(node_attr, node_attr_val).setCData(output));
+      res.writeBody(root.toPrettyString);
+      break;
+
+    case "text":
+      res.writeBody(output);
+      break;
+  }
 }
