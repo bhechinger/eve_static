f71176dc5b92568cfa4bff8171cdd5ffa3a75bf3
Brian Hechinger <bhechinger@sjm.com>
2014-12-29 14:57:22 -0500

    ceil(round(foo, 2)) implementation

--- source/formulas/industry.d
+++ source/formulas/industry.d
@@ -1,11 +1,22 @@
 import std.math;
 
+//auto num = 3.04999999;
+//auto x = cast(int) round(num * 100);
+//auto y = x / 100;
+//auto z = x % 100;
+//writeln("x = ", x);
+//writeln("y = ", y);
+//writeln("z = ", z);
+//if (z > 0) {
+//  y++;
+//}
+//writeln("y = ", y);
+
 double calculateMaterialModifier(int ME = 0, float facility = 1.0, float team1 = 1.0, float team2 = 1.0) {
   auto realME = (100 - ME)/100;
-  return(realME * facility * team1 * team2);
+  return realME * facility * team1 * team2;
 }
 
-ulong calculateMaterials(int runs, ulong baseQuantity, double materialModifier) {
-  //return(max(runs, ceil(round(runs ∗ baseQuantity ∗ materialModifier, 2));
-  return(0);
+real calculateMaterials(int runs, ulong baseQuantity, double materialModifier) {
+  return fmax(runs, ceil(round(runs * baseQuantity * materialModifier)));
 }
